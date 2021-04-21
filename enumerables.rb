336db90eb2520c0a86c0ca230f8a81aacb849914
blakbox23 <boudii30@gmail.com>
2021-04-21 16:04:39 +0300

    Each_with_index solution

--- enumerables.rb
+++ enumerables.rb
@@ -4,4 +4,14 @@ module Enumerable
            yield item
         end
     end
+
+    def my_each_with_index
+        for index in (0...to_a.length)
+            yield to_a[index], index
+        end
+    end
+end
+
+[1,2,3,4,5].my_each_with_index do |item, index|
+    puts "#{item} is index #{index}"
 end
