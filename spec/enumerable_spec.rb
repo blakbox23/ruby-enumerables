b8200b5e6caebc0ce9d09db601cfdcbe73113cf1
Aleksandar Ivezic <a.ivezic@yahoo.com>
2021-05-04 15:40:34 +0200

    Add test for my_each method in case of range input

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -6,6 +6,7 @@ require_relative "../enumerables.rb"
 describe Enumerable do 
     let(:arr) {[1,2,3,4,5]}
     let(:hash) {{'dog' => 'canine', 'cat' => 'feline', 'donkey' => 'asinine', 12 => 'dodecine'}}
+    let(:range) {(3..8)}
     describe "#my_each" do 
         let(:output) {[]}
         context "If we pass a block while calling the method" do 
@@ -19,6 +20,13 @@ describe Enumerable do
             it "iterates trough the hash key/value pairs" do
                 hash.my_each {|key,value| outhash[key] = value}
                 expect(outhash).to eql (hash)
+            end
+        end
+        context "if we are calling my_each on range" do
+            let(:outrange) {Array.new}
+            it "iterates trough the range " do
+                range.my_each {|item| outrange << item}
+                expect(outrange).to eql(range.to_a)
             end
         end
     end
