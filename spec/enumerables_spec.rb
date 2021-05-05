a23241ae7af7ad734cc836cf6d3050a02fd5243c
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-05 09:56:42 +0100

    add #my_each test when block is given for array

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -2,9 +2,14 @@ require './enumerables.rb'
 
 describe Enumerable do
   let(:arr) {[1, 2, 3, 4, 5]}
+
   describe '#my_each' do
     it 'returns enumerator if no block is given' do
       expect(arr.my_each).to be_a(Enumerator)
+    end
+
+    it 'returns an array if block is given' do
+      expect(arr.my_each{ |i| i * 2 }).to eq(arr)
     end
   end
 end
